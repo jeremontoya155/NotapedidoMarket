4aa477e6d66aec411a095cb76546fb2d8660b887
Jeremias <jeremontoya155@gmail.com>
2024-09-11 09:22:50 -0300

    Nota pedido completos

--- middlewares/authMiddleware.js
+++ middlewares/authMiddleware.js
@@ -1,7 +1,25 @@
-exports.isAuthenticated = (req, res, next) => {
-    if (req.session.user) {
-      return next();
-    }
-    res.redirect('/');
-  };
-  
+// Middleware para verificar si el usuario está autenticado
+function isAuthenticated(req, res, next) {
+  if (req.session && req.session.userId) {
+    return next(); // Si el usuario está autenticado, continuar
+  }
+  res.redirect('/login'); // Si no está autenticado, redirigir al login
+}
+
+// Middleware para verificar si el usuario es un comprador
+function isBuyer(req, res, next) {
+  if (req.session && req.session.userRole === 'comprador') {
+    return next(); // Si el usuario es un comprador, continuar
+  }
+  res.redirect('/login'); // Si no, redirigir al login
+}
+
+// Middleware para verificar si el usuario es un administrador
+function isAdmin(req, res, next) {
+  if (req.session && req.session.userRole === 'admin') {
+    return next(); // Si el usuario es admin, continuar
+  }
+  res.redirect('/login'); // Si no, redirigir al login
+}
+
+module.exports = { isAuthenticated, isBuyer, isAdmin };
